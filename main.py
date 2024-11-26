9208babb2272cff5656a33c55685c3d98d02e1d0
mctiantian2501314 <100036451+mctiantian2501314@users.noreply.github.com>
2024-11-26 22:51:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 from public.usage import USAGE as html
 from api.hello import router as hello_router
-from api.random import router as random_router
-from api.v1.groq import router as groq_router
+from api.quji import router as quji_router
 from fastapi import FastAPI
 from fastapi.responses import Response
 app = FastAPI()
 
 app.include_router(hello_router, prefix="/hello")
-app.include_router(random_router, prefix="/random")
-app.include_router(groq_router, prefix="/v1/groq")
-
+app.include_router(quji_router, prefix="/quji")
 
 @app.get("/")
 def _root():
     return Response(content=html, media_type="text/html")
+
+@app.get("/quji/generate-rss")
+async def generate_rss():
+    xml_content = await quji_router.generate_rss()
+    return Response(content=xml_content, media_type="application/rss+xml", charset="utf-8")
